c5c93169ce0949674256c1284aace1a5c0f99ca1
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2025-01-04 14:58:32 -0300

    feat: added new field on bills and removed Nuget.config file to avoid conflicts on nexus

---
+++ Finances/src/FinanceController.Domain.Api/Migrations/20241219224445_TransactionTypeColumnOnBills.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceController.Domain.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class TransactionTypeColumnOnBills : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TransactionType",
+                table: "Bills",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionType",
+                table: "Bills");
+        }
+    }
+}
